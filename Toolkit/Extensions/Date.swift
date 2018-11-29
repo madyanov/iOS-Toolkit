74f874b15c5a999d54d6dd65ac4fb62c52e1911e
Roman Madyanov <romantaken@gmail.com>
2018-11-29 16:33:12 +0300

    Add Date difference extension

--- Toolkit/Extensions/Date.swift
+++ Toolkit/Extensions/Date.swift
@@ -14,3 +14,30 @@ public extension Date {
         return "\(Calendar.current.component(.year, from: self))"
     }
 }
+
+// MARK: - Date defference
+public extension Date {
+    func years(since date: Date) -> Int? {
+        return Calendar.current.dateComponents([.year], from: date, to: self).year
+    }
+
+    func months(since date: Date) -> Int? {
+        return Calendar.current.dateComponents([.month], from: date, to: self).month
+    }
+
+    func days(since date: Date) -> Int? {
+        return Calendar.current.dateComponents([.day], from: date, to: self).day
+    }
+
+    func hours(since date: Date) -> Int? {
+        return Calendar.current.dateComponents([.hour], from: date, to: self).hour
+    }
+
+    func minutes(since date: Date) -> Int? {
+        return Calendar.current.dateComponents([.minute], from: date, to: self).minute
+    }
+
+    func seconds(since date: Date) -> Int? {
+        return Calendar.current.dateComponents([.second], from: date, to: self).second
+    }
+}
