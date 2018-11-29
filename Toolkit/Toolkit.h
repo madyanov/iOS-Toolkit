1ea8e686fae077b77cc5071e3b81d77801f8f00f
Roman Madyanov <romantaken@gmail.com>
2018-11-29 12:57:07 +0300

    Initial Commit

---
+++ Toolkit/Toolkit.h
@@ -0,0 +1,19 @@
+//
+//  Toolkit.h
+//  Toolkit
+//
+//  Created by Roman Madyanov on 29/11/2018.
+//  Copyright © 2018 Roman Madyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Toolkit.
+FOUNDATION_EXPORT double ToolkitVersionNumber;
+
+//! Project version string for Toolkit.
+FOUNDATION_EXPORT const unsigned char ToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Toolkit/PublicHeader.h>
+
+
